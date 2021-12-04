85d13731dc95ddaab6f71815ddc0cf2f6ddb5950
danmirror <danuandreanid@gmail.com>
2021-12-05 03:06:30 +0700

    request bug fix

--- get_data_json/get_data_json.ino
+++ get_data_json/get_data_json.ino
@@ -1,9 +1,18 @@
+/*
+ *  author : Danu Andrean  
+ *  create : 2020
+ *  update : 2021
+ */
+ 
 #include <ESP8266WiFi.h>
 #include <ESP8266HTTPClient.h>
 #include <ArduinoJson.h>
 
+
+int httpCode;
 const char* ssid = "dann";
 const char* password = "danu12345";
+String url = "http://192.168.43.60:8000/api/respon";
 
 void wifiConnecting(){
   while (WiFi.status() != WL_CONNECTED) {
@@ -18,7 +27,7 @@ void wifiConnecting(){
 
 void setup () {
   
-  Serial.begin(115200);
+  Serial.begin(9600);
   WiFi.begin(ssid, password);
   Serial.println("Setting NodeMCU");
   delay(1000);
@@ -26,34 +35,40 @@ void setup () {
 }
  
 void loop() {
-  Serial.print(" stay");
-  String url = "http://192.168.43.60/parsedata/";
+  
   if (WiFi.status() == WL_CONNECTED) { 
-    HTTPClient http;  
-    http.begin(url);  
-    int httpCode = http.GET();                                                       
+    WiFiClient client;
+    HTTPClient http;
+    http.setTimeout(1000);
+    http.begin(client, url);
+    httpCode = http.GET();
+    
     if (httpCode > 0) { 
       String payload = http.getString();
-      Serial.println("HTTP Response Code : ");
+      Serial.print("HTTP Response Code : ");
       Serial.println(httpCode);
       Serial.println("HTTP Response Payload : ");
       Serial.println(payload);
       
       StaticJsonDocument<200> doc;
       DeserializationError error = deserializeJson(doc, payload);
+      
       if (error) {
-    Serial.print(F("deserializeJson() failed: "));
-    Serial.println(error.c_str());
-    return;
-  }
-  const char* sensor = doc["id"];
+        Serial.print(F("deserializeJson() failed: "));
+        Serial.println(error.c_str());
+        return;
+      }
+      const char* sensor = doc["btn1"];
       Serial.println(sensor);
-    http.end();   
-  }else
-  if (WiFi.status() != WL_CONNECTED) {
-    Serial.println("NodeMCU tidak terhubung ke Access Point");
-    wifiConnecting();
+         
+    }
+    http.end();
+    delay(1000);
   }
-  delay(5000);
-}
+  else{
+    if (WiFi.status() != WL_CONNECTED) {
+      Serial.println("NodeMCU tidak terhubung ke Access Point");
+      wifiConnecting();
+    }
+  }
 }
